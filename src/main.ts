9b0d343737768708e1816365591c1dc88422368f
Pongpon Thetthotsaporn <pongpon.work@gmail.com>
2022-11-16 17:38:02 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const whitelist = ['http://localhost:3000', 'http://128.199.178.160:3000'];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
